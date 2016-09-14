8b077a58dbffffea7f3e1cb344d79b22f635ff9f
dayatz <dayat.py@gmail.com>
2016-09-15 00:03:47 +0800

    product serialzer

--- disthrow/apps/product/views.py
+++ disthrow/apps/product/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from apps.distro.models import Distro
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductViewsSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.filter(distro_id=self.kwargs.get('group_pk'))
+
+    def perform_create(self, serializer):
+        distro = Distro.objects.get(pk=self.kwargs.get('distro_pk'))
+        serializer.save(distro=distro)
--- disthrow/apps/urls.py
+++ disthrow/apps/urls.py
@@ -1,7 +1,15 @@
-from rest_framework import routers
+from rest_framework_nested import routers
 from .distro import views as distro_views
+from .product import views as product_views
+
 
 router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'distros', distro_views.DistroViewSet)
 
+product_router = routers.NestedSimpleRouter(
+    router, r'distros', lookup='distro')
+product_router.register(
+    r'products', product_views.ProductViewsSet, base_name='product-distros')
+
 urlpatterns = router.urls
+urlpatterns += product_router.urls
